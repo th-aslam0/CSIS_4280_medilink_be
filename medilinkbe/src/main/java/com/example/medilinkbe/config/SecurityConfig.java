9dd7142530b5957bd1cc932d97cffbc922ed3cbe
th-aslam <tamoor.h.aslam@ooma.com>
2025-04-08 14:34:24 -0700

    Fix auth in resource server

--- medilinkbe/src/main/java/com/example/medilinkbe/config/SecurityConfig.java
+++ medilinkbe/src/main/java/com/example/medilinkbe/config/SecurityConfig.java
@@ -2,25 +2,24 @@ package com.example.medilinkbe.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-            .csrf().disable()
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/public/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .oauth2Login()
-            .and()
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt())
-            .build();
-    }
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		return http.authorizeHttpRequests(
+
+				authorize -> authorize.requestMatchers(HttpMethod.GET, "/api/football/teams/**")
+						.hasAuthority("SCOPE_football:read").requestMatchers(HttpMethod.POST, "/api/football/teams/**")
+						.hasAuthority("SCOPE_football:admin").anyRequest().authenticated()
+
+		).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
+
+	}
 }
