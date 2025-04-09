1e7c27eea9b83720e8a755a1233618befb65ce05
th-aslam <tamoor.h.aslam@ooma.com>
2025-04-08 19:22:05 -0700

    Permitted all API calls for development phase & added auth server

--- medilinkbe/src/main/java/com/example/medilinkbe/config/SecurityConfig.java
+++ medilinkbe/src/main/java/com/example/medilinkbe/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,14 +13,12 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    http
+	        .authorizeHttpRequests(authorize -> authorize
+	            .anyRequest().permitAll()
+	        )
+	        .csrf(AbstractHttpConfigurer::disable); // disable CSRF for simplicity
 
-		return http.authorizeHttpRequests(
-
-				authorize -> authorize.requestMatchers(HttpMethod.GET, "/api/football/teams/**")
-						.hasAuthority("SCOPE_football:read").requestMatchers(HttpMethod.POST, "/api/football/teams/**")
-						.hasAuthority("SCOPE_football:admin").anyRequest().authenticated()
-
-		).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
-
+	    return http.build();
 	}
 }
