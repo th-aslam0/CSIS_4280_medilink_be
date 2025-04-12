209b61909882e280c39f48cf8249af77c1d79a94
Nam <hoangnam611@gmail.com>
2025-04-11 18:05:54 -0700

    Nam added swagger notation for documentation for DoctorController

--- medilinkbe/src/main/java/com/example/medilinkbe/controller/DoctorController.java
+++ medilinkbe/src/main/java/com/example/medilinkbe/controller/DoctorController.java
@@ -18,6 +18,12 @@ import com.example.medilinkbe.model.Doctor;
 import com.example.medilinkbe.repository.DoctorRepository;
 import com.example.medilinkbe.service.DoctorService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RequestMapping("/api")
 @RestController
 public class DoctorController {
@@ -25,7 +31,17 @@ public class DoctorController {
 	@Autowired
 	private DoctorService doctorService;
 	
-		
+	
+	
+	@Operation(summary = "Get all doctors")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Found a list of doctors", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Doctor.class)) }),
+//			@ApiResponse(responseCode = "400", description = "Invalid ID supplied", 
+//					content = @Content),
+//			@ApiResponse(responseCode = "404", description = "Player not found", 
+//			content = @Content)
+			})	
 	@GetMapping("/doctors")
 	public ResponseEntity<?> listDoctors() {
 		List<Doctor> doctors = doctorService.listDoctors();
@@ -37,6 +53,15 @@ public class DoctorController {
 	}
 	
 	// READ ONE
+	@Operation(summary = "Get a doctor by Id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Found doctor by Id", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Doctor.class)) }),
+			@ApiResponse(responseCode = "400", description = "Invalid ID supplied", 
+					content = @Content),
+			@ApiResponse(responseCode = "404", description = "Doctor not found", 
+			content = @Content)
+			})
     @GetMapping("/doctors/{id}")
     public ResponseEntity<?> getDoctorById(@PathVariable("id") String id) {
         Doctor doctor = doctorService.getDoctorById(id);
@@ -48,6 +73,16 @@ public class DoctorController {
         }
     }	
 
+	
+	@Operation(summary = "Add a new doctor")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successfully added a doctor", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Doctor.class)) }),
+//			@ApiResponse(responseCode = "400", description = "Invalid ID supplied", 
+//					content = @Content),
+//			@ApiResponse(responseCode = "404", description = "Doctor not found", 
+//			content = @Content)
+			})
 	@PostMapping("/doctors")
 	public ResponseEntity<?> createDoctor(@RequestBody Doctor doctor) {
 		try {
@@ -58,12 +93,29 @@ public class DoctorController {
 		}
 	}
 	
+	@Operation(summary = "Delete a doctor by Id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successfully deleted a doctor by Id", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Doctor.class)) }),
+			@ApiResponse(responseCode = "400", description = "Invalid ID supplied", 
+					content = @Content),
+			@ApiResponse(responseCode = "404", description = "Doctor not found", 
+			content = @Content)
+			})
 	@DeleteMapping("doctors/{id}")
 	public void deleteDoctor(@PathVariable("id") String id) {
 		doctorService.deleteDoctor(id);
 	}
 	
-	
+	@Operation(summary = "Update an existing doctor by Id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successfully updated a doctor by Id", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Doctor.class)) }),
+			@ApiResponse(responseCode = "400", description = "Invalid ID supplied", 
+					content = @Content),
+			@ApiResponse(responseCode = "404", description = "Doctor not found", 
+			content = @Content)
+			})
 	@PutMapping("/doctors/{id}")
 	public void updateDoctor(@PathVariable("id") String id, @RequestBody Doctor doctor) {
 		doctorService.updateDoctor(id,doctor);
