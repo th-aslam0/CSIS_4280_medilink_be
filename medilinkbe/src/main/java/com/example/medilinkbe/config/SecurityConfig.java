05858ce83b1d64f91858f9e83080a5507db25cdb
th-aslam <tamoor.h.aslam@ooma.com>
2025-04-20 22:46:24 -0700

    start

--- medilinkbe/src/main/java/com/example/medilinkbe/config/SecurityConfig.java
+++ medilinkbe/src/main/java/com/example/medilinkbe/config/SecurityConfig.java
@@ -2,23 +2,20 @@ package com.example.medilinkbe.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	    http
-	        .authorizeHttpRequests(authorize -> authorize
-	            .anyRequest().permitAll()
-	        )
-	        .csrf(AbstractHttpConfigurer::disable); // disable CSRF for simplicity
-
-	    return http.build();
-	}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+            .build();
+    }
 }
